c616ffa1f226091dafa7d0f839c975e0a597ace8
Juan I Pulleiro <juan.i.pulleiro@gmail.com>
2023-05-15 22:44:24 -0300

    primeros cambios

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,44 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+  <style>
+  </style>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <ion-app>
+    <ion-header>
+      <RouterLink to="/">Home |</RouterLink>
+      <RouterLink to="/habitaciones">Habitaciones |</RouterLink>
+      <RouterLink to="/misReservas">Reservas |</RouterLink>
+      <RouterLink to="/login">Login |</RouterLink>
+      <RouterLink to="/">Logout</RouterLink>
+    </ion-header>
+    <RouterView />
+  </ion-app>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+import {IonApp, IonHeader} from '@ionic/vue'
+export default {
+  components: {IonApp, IonHeader}
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+import { defineStore } from 'pinia'
+export const useLoginStore = defineStore('login', {
+  state: () => {
+    return { isLogin: false }
+  },
+  actions: {
+    logout() {
+      this.isLogin = false
+    },
+    login() {
+      this.isLogin = true
+    },
+    setup() {
+      const store = useLoginStore();
+      const { isLogin } = storeToRefs(store)
+      return { isLogin }
+    }
+  },
+})
+</script>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,35 @@
+<template>
+  <ion-page>
+    <h2>Login</h2>
+    <ion-button @click="logear">Login</ion-button>
+  </ion-page>
+</template>
+
+<script>
+import { IonPage, IonButton } from "@ionic/vue";
+import { useLoginStore } from "../stores/login";
+
+export default {
+  components: { IonPage, IonButton },
+  setup() {
+    const store = useLoginStore();
+    const { login } = store;
+    return { login };
+  },
+  methods: {
+    logear() {
+        // hardcodeo hasta la classe que viene
+        // proxima clase conexion con backend
+        if (this.usuario.email=="test@test.com" && this.usuario.passw=="123456") {
+          this.login()
+          this.$router.push('system')
+        } else {
+          alert('Credenciales incorrectas')
+        }
+    }
+  }
+};
+</script>
+
+<style>
+</style>
