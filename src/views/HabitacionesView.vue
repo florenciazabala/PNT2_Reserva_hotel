8feb1fe29e236d6e49069f02fb51e9a90d2b418c
PedroGutierrez <pedro_44mas@hotmail.com>
2023-06-12 21:07:37 -0300

    permisos para borrar o modificar habitaciones

--- src/views/HabitacionesView.vue
+++ src/views/HabitacionesView.vue
@@ -1,17 +1,24 @@
+
 <template>
   <ion-page>
-    <ion-content style="
-          background: url('../src/assets/hotel5.jpg') no-repeat center center
-            fixed;
-          background-size: cover;
-        ">
+    <ion-content
+      style="
+        background: url('../src/assets/hotel5.jpg') no-repeat center center
+          fixed;
+        background-size: cover;
+      "
+    >
       <h2>Habitaciones Disponibles</h2>
       <ion-list class="listNegro" v-for="e in lista" :key="e.id">
         <ion-card color="dark">
           <ion-card-header>
-            <ion-card-title>Habitacion numero #{{ e.NumeroDeHabitacion }}</ion-card-title>
-            <ion-card-subtitle>Cantidad de personas máximo para la habitacion:
-              {{ e.cantPersonas }}</ion-card-subtitle>
+            <ion-card-title
+              >Habitacion numero #{{ e.NumeroDeHabitacion }}</ion-card-title
+            >
+            <ion-card-subtitle
+              >Cantidad de personas máximo para la habitacion:
+              {{ e.cantPersonas }}</ion-card-subtitle
+            >
           </ion-card-header>
 
           <ion-card-content>
@@ -20,25 +27,49 @@
             <ion-text>| Precio $</ion-text>{{ e.Precio }}
             <ion-list class="listNegro">
               <ion-item color="dark">
-                <ion-thumbnail slot="start">
+                <ion-thumbnail >
                   <img alt="Foto habitación" :src="e.imagen" />
                 </ion-thumbnail>
                 <ion-label>Item</ion-label>
               </ion-item>
             </ion-list>
           </ion-card-content>
-          <ion-button color="warning" router-link="/reservaHabitacion" router-direction="back">Reservar
-            Habitación</ion-button>
-          <ion-button color="warning"  @click="eliminar(e.id)" fill="clear">Eliminar</ion-button>
+          <ion-button
+            color="warning"
+            router-link="/reservaHabitacion"
+            router-direction="back"
+            >Reservar Habitación</ion-button
+          >
+          <ion-button
+            color="warning"
+            v-if="isLogin && hasPermissions('config')"
+            @click="eliminar(e.id)"
+            fill="clear"
+            >Eliminar</ion-button
+          >
 
           <p id="ejemplo"></p>
-          <ion-button color="warning" @click="modificar(e.id)" fill="clear">Modificar</ion-button>
+          <ion-button
+            color="warning"
+            v-if="isLogin && hasPermissions('config')"
+            @click="modificar(e.id)"
+            fill="clear"
+            >Modificar</ion-button
+          >
         </ion-card>
       </ion-list>
-      <ion-button color="light" fill="outline" @click="cargarLista">Cargar Lista</ion-button>
-      <ion-button color="light" fill="outline" @click="agregaraLista">Agregar</ion-button>
-      <ion-button color="light" fill="outline" @click="ordenarLista">Ordenar</ion-button>
-      <ion-button color="light" fill="outline" @click="iraHome">Ir a home</ion-button>
+      <ion-button color="light" fill="outline" @click="cargarLista"
+        >Cargar Lista</ion-button
+      >
+      <ion-button color="light" fill="outline" @click="agregaraLista"
+        >Agregar</ion-button
+      >
+      <ion-button color="light" fill="outline" @click="ordenarLista"
+        >Ordenar</ion-button
+      >
+      <ion-button color="light" fill="outline" @click="iraHome"
+        >Ir a home</ion-button
+      >
     </ion-content>
   </ion-page>
 </template>
@@ -61,8 +92,13 @@ import {
   IonItem,
   IonText,
 } from "@ionic/vue";
+
 import axios from "axios";
-import { useLoginStore } from "../stores/login";
+import { RouterLink, RouterView, useRouter } from "vue-router";
+import {IonApp, IonHeader, IonRouterOutlet } from '@ionic/vue'
+import { storeToRefs } from "pinia";
+import {useLoginStore} from '../stores/login'
+
 import habitaciones from "../services/habitaciones";
 
 export default {
@@ -83,7 +119,17 @@ export default {
     IonItem,
     IonText,
   },
+
+  setup() {
+    const router = useRouter();
+    const store = useLoginStore();
+    const { isLogin, user } = storeToRefs(store);
+    const { hasPermissions } = store;
+    return { isLogin, user, hasPermissions, router};
+  },
+  
   methods: {
+    
     iraHome() {
       this.$router.push("/");
     },
@@ -120,7 +166,7 @@ export default {
           mensaje = "Has clickado Cancelar";
         }
         document.getElementById("ejemplo").innerHTML = mensaje;
-        
+
         await this.cargarLista();
       } catch (e) {
         alert("Se produjo un error");
